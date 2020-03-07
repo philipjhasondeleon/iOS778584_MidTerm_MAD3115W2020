90acc310fe01682f7312b42f739635b7de4858c1
jhay <jhasondeleon@yahoo.com>
2020-03-06 21:03:35 -0500

    [Philip] alert message added

--- iOS778584_MidTerm_MAD3115W2020/ViewController.swift
+++ iOS778584_MidTerm_MAD3115W2020/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPassWord: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func btnSignIn(_ sender: UIButton)
+        {
+            if let bundlePath = Bundle.main.path(forResource: "UserInfo", ofType: "plist")
+            {
+                if let dictionary = NSMutableDictionary(contentsOfFile: bundlePath)
+                {
+                    if let users = dictionary["users"] as? [[String:String]]
+                    {
+                        var flag = false
+                        for userList in users
+                        {
+                            if userList["username"] == txtUserName.text && userList["password"] == txtPassWord.text
+                            {
+                                flag = true
+                                
+                            }
+                            if flag == true{
+                            print("Valid User")
+                            }else  {
+                                      let alertController = UIAlertController(title: "Login Failed!", message:
+                                      "Username or Password do not match", preferredStyle: .actionSheet)
+                                      alertController.addAction(UIAlertAction(title: "Try Again", style: .cancel))
+                                    
+                                      
+                                      
+                                      self.present(alertController, animated: true, completion: nil)
+                                  }
+                              }
+                        }
+                }
+        }
+    }
 }
+    
 
