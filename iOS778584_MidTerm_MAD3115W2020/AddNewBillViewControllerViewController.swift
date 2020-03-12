0565c399f40139d7f7c73a40cb123ecb322610b5
philip <jhasondeleon@yahoo.com>
2020-03-11 21:18:01 -0400

    [Philip] AddNewBillViewController added

---
+++ iOS778584_MidTerm_MAD3115W2020/AddNewBillViewControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewBillViewControllerViewController.swift
+//  iOS778584_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-11.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class AddNewBillViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
