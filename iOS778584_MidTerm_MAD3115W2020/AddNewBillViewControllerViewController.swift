fbeccd2e8e1acbae9eaaf065d6f2e998a42a138b
philip <jhasondeleon@yahoo.com>
2020-03-12 21:30:33 -0400

    [Philip] bill type picker view added in addnewbillviewcontroller

--- iOS778584_MidTerm_MAD3115W2020/AddNewBillViewControllerViewController.swift
+++ iOS778584_MidTerm_MAD3115W2020/AddNewBillViewControllerViewController.swift
@@ -8,59 +8,126 @@
 
 import UIKit
 
-class AddNewBillViewControllerViewController: UIViewController, UITextFieldDelegate {
+enum PickerType:Int {
+    case BillType = 0
+}
+class AddNewBillViewControllerViewController: UIViewController {
 
     @IBOutlet weak var textField_Date: UITextField!
-    
-        var datePicker : UIDatePicker!
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-        }
-        
-        //MARK:- textFiled Delegate
-        func textFieldDidBeginEditing(_ textField: UITextField) {
-            self.pickUpDate(self.textField_Date)
-        }
-        
-        //MARK:- Function of datePicker
-        func pickUpDate(_ textField : UITextField){
-            
-            // DatePicker
-            self.datePicker = UIDatePicker(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
-            self.datePicker.backgroundColor = UIColor.white
-            self.datePicker.datePickerMode = UIDatePicker.Mode.date
-            textField.inputView = self.datePicker
-            
-            // ToolBar
-            let toolBar = UIToolbar()
-            toolBar.barStyle = .default
-            toolBar.isTranslucent = true
-            toolBar.tintColor = .red//UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
-            toolBar.sizeToFit()
-            
-            // Adding Button ToolBar
-            let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewBillViewControllerViewController.doneClick))
-            let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-            let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewBillViewControllerViewController.cancelClick))
-            toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
-            toolBar.isUserInteractionEnabled = true
-            textField.inputAccessoryView = toolBar
-            
-        }
-        
-        // MARK:- Button Done and Cancel
-        @objc func doneClick() {
-            let dateFormatter1 = DateFormatter()
-            dateFormatter1.dateStyle = .medium
-            dateFormatter1.timeStyle = .none
-            textField_Date.text = dateFormatter1.string(from: datePicker.date)
-            textField_Date.resignFirstResponder()
-        }
-        
-        @objc func cancelClick() {
-            textField_Date.resignFirstResponder()
-        }
 
+    @IBOutlet weak var pickerBillType: UIPickerView!
+    @IBOutlet weak var textBillType: UITextField!
+    
+    var billType = ["Hydro", "Internet", "Mobile"]
+    
+    var datePicker : UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.pickerBillType.delegate = self
+        self.pickerBillType.dataSource = self
+        
     }
+    
+    //MARK:- textFiled Delegate
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickUpDate(self.textField_Date)
+    }
+    
+    //MARK:- Function of datePicker
+    func pickUpDate(_ textField : UITextField){
+        
+        // DatePicker
+        self.datePicker = UIDatePicker(frame:CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        self.datePicker.datePickerMode = UIDatePicker.Mode.date
+        textField.inputView = self.datePicker
+        
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .red//UIColor(red: 92/255, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(AddNewBillViewControllerViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(AddNewBillViewControllerViewController.cancelClick))
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
+        
+    }
+    
+    // MARK:- Button Done and Cancel
+    @objc func doneClick() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        textField_Date.text = dateFormatter1.string(from: datePicker.date)
+        textField_Date.resignFirstResponder()
+    }
+    
+    @objc func cancelClick() {
+        textField_Date.resignFirstResponder()
+    }
+    
+
+}
+
+extension AddNewBillViewControllerViewController:UIPickerViewDelegate
+{
+    //Set the Value for each row
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        //return self.courseArray[row]
+            return self.billType[row]
+    }
+    
+    //Fetch the selected values
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //self.lblCourseName.text = self.courseArray[row]
+        
+        let strBillTypeName = self.billType[pickerView.selectedRow(inComponent: PickerType.BillType.rawValue)]
+    }
+}
+
+extension AddNewBillViewControllerViewController:UIPickerViewDataSource
+{
+    
+    //No of components
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //No. of items in picker view
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+            return self.billType.count
+        
+    }
+}
+
+extension AddNewBillViewControllerViewController: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if !textField.text!.isEmpty
+        {
+            if textField.tag == 0
+            {
+                self.billType.append(textField.text!)
+            }
+            
+            textField.resignFirstResponder()
+            self.pickerBillType.reloadComponent(textField.tag)
+        }
+        return true
+    }
+    
+    
+}
+
+
+    
+        
